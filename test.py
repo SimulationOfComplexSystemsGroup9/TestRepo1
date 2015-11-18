e091f69c81a062930ee4f3ed1d42ed26522d9353
RafalPiwowarczyk <rafalp@student.chalmers.se>
2015-11-18 15:12:10 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,4 @@ for i in range(10):
     print("Ladida")
     print("ladoda")
     print("hyllan är på väggen!")
+    print("Grzegorz Brzeczyszczykiewicz - hardest polish name and sunrame to pronounce for foreigners")
